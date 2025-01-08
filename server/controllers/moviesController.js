0171f73d1eb40fc6cb1687666d79e777b7a359df
Arela-Cristina <instagrandine@gmail.com>
2025-01-08 17:17:00 +0100

    feat: create controllers

---
+++ server/controllers/moviesController.js
@@ -0,0 +1,21 @@
+
+//salviamo la istanza del collegamento server e db 16
+const connection = require('../data/db')
+
+
+// creo i controllers con la struttura REST 17
+function index(req, res) {
+
+    res.json({
+        message: 'movies index',
+    })
+}
+
+
+function show(req, res) {
+    res.json({
+        message: 'movies show',
+    })
+}
+
+module.export = { index, show } // esportiamo i controller.
