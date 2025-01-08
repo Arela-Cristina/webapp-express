31847bf088dd104f6d67c73050b3d9b1b4f6185e
Arela-Cristina <instagrandine@gmail.com>
2025-01-08 16:29:04 +0100

    import middleware notFound

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 console.log('cri')
 const express = require('express'); //importare express  1
 const app = express(); // salviamo istanza della funzione express  2
-const port = 3000 //dichiariammo la porta  3
+const port = 3000; //dichiariammo la porta  3
+const notFound = require ('./middlewares/notFound') //importiamo middleware notFound 9
 
 
 // importiamo middleware per rendere publiche le immagini  6
@@ -12,7 +13,8 @@ app.get('/', (req, res) => {
     res.send(`Server operativo`)
 })
 
-
+//registriamo il middleware not Found 10
+app.use(notFound) 
 
 //express ascolta la porta per fare adare il server   4
 app.listen(port, () => {
