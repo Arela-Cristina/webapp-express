7ce2f9513def9015e4b63fe83577ae2339c509a1
Arela-Cristina <instagrandine@gmail.com>
2025-01-08 18:48:23 +0100

    fix error

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const app = express(); // salviamo istanza della funzione express  2
 const port = 3000; //dichiariammo la porta  3
 const notFound = require('./middlewares/notFound') //importiamo middleware notFound 9
 const errorsHandler = require('./middlewares/errorsHandler')  //importiamo middleware notFound 11
+const movieRouter = require('./routers/moviesRouter') //importiamo le rotte 24
 
 // importiamo middleware per rendere publiche le immagini  6
 app.use(express.static('public'))
@@ -13,7 +14,11 @@ app.get('/', (req, res) => {
     res.send(`Server operativo`)
 })
 
+//registrare le rotte movies 25
+app.use('/api/movies', movieRouter)
 
+
+//MIDDLEWARE
 app.use(errorsHandler)//registriamo il middleware not Found 12
 app.use(notFound) //registriamo il middleware not Found 10
 
