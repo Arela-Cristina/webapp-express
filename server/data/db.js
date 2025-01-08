0de322388a7e0cd3f03332f745feb798d83de910
Arela-Cristina <instagrandine@gmail.com>
2025-01-08 16:57:58 +0100

    feat: connect server whit DB

---
+++ server/data/db.js
@@ -0,0 +1,18 @@
+//importiamo mysql2   12
+const mysql = require('mysql2')
+
+
+const connection = mysql.createConnection({ //dichiariamo i nostri dati di workbench con la funzione createConnection 13
+    host: 'localhost',
+    user: 'root',
+    password: 'Jcryxc8c@',
+    database: 'movies_db'
+})
+
+connection.connect((err) => { // chiamata to connection tramite callback  14
+    if (err) throw err
+
+    console.log('Collegamento MYSQL ok')
+})
+
+module.exports = connection
