af500170f47a1c25bf5912958e639da6a14ac7ee
Arela-Cristina <instagrandine@gmail.com>
2025-01-08 16:34:57 +0100

    import middleware errorsHandler

--- server/app.js
+++ server/app.js
@@ -2,8 +2,8 @@ console.log('cri')
 const express = require('express'); //importare express  1
 const app = express(); // salviamo istanza della funzione express  2
 const port = 3000; //dichiariammo la porta  3
-const notFound = require ('./middlewares/notFound') //importiamo middleware notFound 9
-
+const notFound = require('./middlewares/notFound') //importiamo middleware notFound 9
+const errorsHandler = require('./middlewares/errorsHandler')  //importiamo middleware notFound 11
 
 // importiamo middleware per rendere publiche le immagini  6
 app.use(express.static('public'))
@@ -13,8 +13,9 @@ app.get('/', (req, res) => {
     res.send(`Server operativo`)
 })
 
-//registriamo il middleware not Found 10
-app.use(notFound) 
+
+app.use(errorsHandler)//registriamo il middleware not Found 12
+app.use(notFound) //registriamo il middleware not Found 10
 
 //express ascolta la porta per fare adare il server   4
 app.listen(port, () => {
