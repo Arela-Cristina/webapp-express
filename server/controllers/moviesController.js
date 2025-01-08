1a3cad42418845eed40c1e3121b93e7bdda7d9d7
Arela-Cristina <instagrandine@gmail.com>
2025-01-08 19:22:56 +0100

    feat: structure queries index and show  functionalities

--- server/controllers/moviesController.js
+++ server/controllers/moviesController.js
@@ -4,18 +4,45 @@ const connection = require('../data/db')
 
 
 // creo i controllers con la struttura REST 17
+
+//index
 function index(req, res) {
-
-    res.json({
-        message: 'movies index',
+    //esseguiamo prima la query su workbench per poi fare la query giusta ed avere tutti i film 26
+    let sql = `SELECT * FROM movies`
+    connection.query(sql, (err, movies) => {
+        if (err) return res.status(500).json({ message: err.message })
+        res.json(movies)
     })
 }
 
-
+//show
 function show(req, res) {
-    res.json({
-        message: 'movies show',
+  //recupero el id di ogni movie 27
+    const id = req.params.id
+   
+    //query per avere un film 28
+    const sql = `SELECT * FROM movies WHERE id = ?`
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ message: err.message })
+        if (results.length === 0)
+            return res.status(404).json({
+                error: 'Not Found',
+                message: 'Movie not found',
+            })
+
+        const movie = results[0]
+
+        const sql = `SELECT * FROM reviews WHERE movie_id = ?` //query per avere la review di un film 29
+
+        connection.query(sql, [id], (err, results) => {
+            if (err) return res.status(500).json({ message: err.message })
+
+            movie.reviews = results
+           
+            res.json(movie)
+        })
     })
 }
 
-module.exports = { index, show } // esportiamo i controller.
+module.exports = { index, show } 
